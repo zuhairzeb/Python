5acdfb28d2066e724d4ca0666fd124943238e288
Muhammad Zuhair Zeb <zebzuhair71@gmail.com>
2025-02-20 09:55:02 +0500

    Python 2025

---
+++ Basics/04_02_Comparison_op.py
@@ -0,0 +1,12 @@
+# Comparison Operators aise operators hain jo do cheezon (numbers, values ya variables) ka mukabla (comparison) 
+# karte hain aur true ya false ka result dete hain. Yeh zyada tar if conditions me istemal hote hain.
+
+a = 10
+b = 20
+
+print(a == b)  # False
+print(a != b)  # True
+print(a > b)   # False
+print(a < b)   # True
+print(a >= b)  # False
+print(a <= b)  # True
---
+++ Basics/04_03_Logical_Op.py
@@ -0,0 +1,20 @@
+# Logical Operators aise operators hain jo do ya zyada conditions ko aik sath check karte hain aur
+#  True ya False ka result dete hain.
+#  Yeh zyadatar if conditions me istemal hote hain.
+
+# and
+
+print(5 > 3 and 10 > 5)  # True
+print(5 > 3 and 10 < 5)  # False
+
+
+# or
+
+print(5 > 3 or 10 < 5)  # True
+print(5 < 3 or 10 < 5)  # False
+
+
+# not
+
+print(not (5 > 3))  # False
+print(not (5 < 3))  # True
