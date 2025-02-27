76d2e5b43d43b53983f8a982e68ab386e6a4baf2
Muhammad Zuhair Zeb <zebzuhair71@gmail.com>
2025-02-27 13:19:30 +0500

    Python 2025

---
+++ Basics/06_00_Control Flow.py
@@ -0,0 +1,2 @@
+# Control Flow ka matlab hai program kis tarteeb (order) me chalega. 
+# Matlab computer ko batana ke pehle kya kare, phir kya kare, aur kis condition pe kya kare.
---
+++ Basics/06_01_ConditionalStat_cf.py
@@ -0,0 +1,23 @@
+# Conditional Statements: if, elif, else
+  # Conditional statements ka matlab hai kisi condition (shart) ke mutabiq faisla lena. 
+  # Python me if, elif, else ka use hota hai decision making ke liye.
+
+    # if: Executes a block of code if the condition is true.
+
+    #  elif: Checks another condition if the previous condition(s) were false.
+
+    #   else: Executes a block of code if all previous conditions were false.
+
+       
+# Example:
+
+age=int(input("enter your age :-"))
+if age < 18: 
+    print("You are a minor.")
+elif age == 18:
+   print("You just turned 18!")
+else: 
+ print("You are an adult.")
+
+
+
