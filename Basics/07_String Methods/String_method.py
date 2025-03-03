775218c579674ae5d94652939598bfc2524a698b
Muhammad Zuhair Zeb <zebzuhair71@gmail.com>
2025-03-03 14:50:55 +0500

    Python 2025

---
+++ Basics/07_String Methods/String_method.py
@@ -0,0 +1,34 @@
+text = "My name is Muhammad Zuhair zeb"
+print(text.upper())
+
+# # Lower() method
+
+text = "My name is Muhammad Zuhair zeb"
+print(text.lower())
+
+# # Title() method
+
+text = "My name is Muhammad Zuhair zeb"
+print(text.title())
+
+# strip() method
+
+text = "  My name is Muhammad Zuhair zeb   "
+print(text.strip())
+
+# Right strip() method
+
+text = "  My name is Muhammad Zuhair zeb   "
+print(text.rstrip())
+
+# Find() method
+text = "i am a student of Artificial Intelligence"
+print(text.find("student"))
+
+# Replace() method
+text = "My name is Zuhair"
+print(text.replace("Zuhair", "Zuhee"))
+
+# Find value in string
+text = "My name is Zuhair"
+print("Zuhee" in text)
